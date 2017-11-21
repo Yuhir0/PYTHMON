b9dcf507cba3e13e4fc5e2a4f4beb073b98c70de
Yuhiro <powerofyuhiro@gmail.com>
2017-11-21 22:02:47 +0100

    Obligació a canviar si el pythmon del jugador es devilita i millora d'alguna animació

--- Pythmon.py
+++ Pythmon.py
@@ -22,6 +22,7 @@ def clear():
         raise "No es pot netegar la pantalla"
         print  "<-No s'ha pogut netejar la pantalla->"
 
+
 """ 
 Aqui estan tos els pythmon amb el seguient ordre Nom, Tipus, Vida, Atac, Defensa,
 Velocitat, Atac1, Atac2, Atac3.
@@ -46,9 +47,12 @@ pythmon = [["Bulbasaur","Planta",231,147,134,126],
 		["Lapras","Aigua",401,226,196,156],
 		["Magikarp","Aigua",999,999,999,1]]
 
-# Es l'apariencia de cada moment del joc.
+
+# Es l'apariencia de cada moment del joc (animacions)
 def pantalla(text,escena):
 	escriu = " "
+	
+	# Encara no hi ha combat, per tant hi ha una pantalla en negre i només es fa l'animació del text
 	if escena == 0:
 		for i in text:
 			escriu += i
@@ -56,13 +60,14 @@ def pantalla(text,escena):
 			print "+-----------------------------------------------+"
 			print escriu
 			print "+-----------------------------------------------+"
-			time.sleep(0.05)
+			time.sleep(0.03)
 			clear()
 		print "\n\n\n\n\n\n\n"
 		print "+-----------------------------------------------+"
 		print "",text
 		print "+-----------------------------------------------+"
-		
+	
+	# L'enemic treu un pythmon, animació de text + sortida al camp del pythmon
 	elif escena == 1:
 		for i in text:
 			escriu += i
@@ -71,11 +76,12 @@ def pantalla(text,escena):
 			print "+-----------------------------------------------+"
 			print escriu
 			print "+-----------------------------------------------+"
-			time.sleep(0.05)
+			time.sleep(0.03)
 			
 		
 		time.sleep(0.3)
-			
+		
+		# Animació de quan el contrari treu un Pythmon
 		for i in range(4):
 			clear()
 			for j in range(i):
@@ -87,6 +93,7 @@ def pantalla(text,escena):
 			print "+-----------------------------------------------+"
 			time.sleep(0.2)
 	
+	# El jugador treu un pythmon, animació de text + sortida al camp del pythmon
 	elif escena == 2:	
 		for i in text:
 			escriu += i
@@ -96,10 +103,11 @@ def pantalla(text,escena):
 			print "+-----------------------------------------------+"
 			print escriu
 			print "+-----------------------------------------------+"
-			time.sleep(0.05)
+			time.sleep(0.03)
 		
 		time.sleep(0.3)
 		
+		# Animació de quan el jugador treun un Pythmon
 		for i in range(5,-1,-1):
 			clear()
 			print "\t\t\t" + encombat[1][0] + " | vida:",encombat[1][2]
@@ -112,6 +120,7 @@ def pantalla(text,escena):
 			print "+-----------------------------------------------+"
 			time.sleep(0.2)
 	
+	# Els dos pythmon estan en combat, només es fa l'animació del text
 	elif escena == 3:
 		for i in text:
 			escriu += i
@@ -122,19 +131,23 @@ def pantalla(text,escena):
 			print "+-----------------------------------------------+"
 			print escriu
 			print "+-----------------------------------------------+"
-			time.sleep(0.05)
+			time.sleep(0.03)
 
 
 # Es defineixen els atacs
-def Atac(tipus,potencia,encombat,torn):	
+def Atac(tipus,potencia,encombat,torn):
+	
+	# Atacs molt efectius
 	if tipus == "Planta" and encombat[1][1] == "Aigua" or tipus == "Foc" and encombat[1][1] == "Planta" or tipus == "Aigua" and encombat[1][1] == "Foc":
 		mal = (potencia + encombat[0][3] - encombat[1][4]) * 2
 		pantalla(encombat[0][0] + " ha usat " + encombat[0][torn] + ", es molt efectiu.", 3)
-		
+	
+	# Atacs poc efectius
 	elif tipus == "Planta" and encombat[1][1] == "Foc" or tipus == "Foc" and encombat[1][1] == "Aigua" or tipus == "Aigua" and encombat[1][1] == "Planta":
 		mal = (potencia + encombat[0][3] - encombat[1][4]) * 0.5
 		pantalla(encombat[0][0] + " ha usat " + encombat[0][torn] + ", no es gaire efectiu.", 3)
 		
+	# Atacs efectius
 	else:
 		mal = potencia + encombat[0][3] - encombat[1][4]
 		pantalla(encombat[0][0] + " ha usat " + encombat[0][torn] + ".", 3)
@@ -142,67 +155,77 @@ def Atac(tipus,potencia,encombat,torn):
 
 
 mipythmon = [[""],[""],[""]] # Espai per als pythmon del jugador		
+
 # Avans de iniciar el joc et demana un nom
-print "Quin es el tu nom?"
+pantalla("Quin es el tu nom?",0)
 Nom = raw_input()
+
+combat = 1 # Identifica la ronda per la que va el jugador
 
 """
 (En estat de prova)D'un fitxer xml agafem les dades i comprovem si el nom que ha inreoduit
 l'usuari hi es al fitxer xml i preguntar-li si vol continuar la partida
 anterior 
 """
-"""save = etree.parse('partida.xml')
-jugadors = save.findall("jugador")
-lj = len(jugadors)
+
+save = etree.parse('partida.xml') # Carreguem el fixer 'partida.xml' que conte les dades dels jugadrs
+jugadors = save.findall("jugador") # Trobem tots el jugadors
+
+lj = len(jugadors) # Averigüem quants jugadors són
+
 for i in range(lj):
 	if Nom == jugadors[i].attrib["nom"]:
 		print "Tens una partida començada"
-		p = jugadors[i].findall("pythmon")
-		lp = len(p)
-		for n in range(lp):
-			mipythmon[n] = p[n].attrib["num"]
-		c = jugadors[i].findall("combat")
-		combat = c[0].attrib["cont"]
-		joc(combat,mipythmon)
+		p = jugadors[i].findall("pythmon") # Trobem tots els pythmon si el jugador esta registrat
+		
+		for n in range(3):
+			mipythmon[n] = pythmon[int(p[n].attrib["num"])]
+		c = jugadors[i].findall("combat") # Trobem els combats
+		combat = int(c[0].attrib["cont"]) # El convertim a enter i li assignem a combat
 		break
 	elif i == lj-1 and Nom != jugadors[lj-1].attrib["nom"]:
 		print "Benvingut a la nostra torre Pythmon, guanya\na tots entrenadores i aconsegueix la vicotria"
 
-if combat >= 10:
-	exit()
-"""
+#if combat > 10: # Si el jugador te mes de 10 combats ja el ha fet tots
+#	exit()
+print combat
+if combat > 1 and combat < 10: # Si el jugador ha fet més d'un combat indicara la ronda y pasara al while del joc.
+	print "Vas per la ronda",combat
+	
+else:
+	# Llista els pythmon en dos columnes per a que el jugador els pugui veure
+	for i in range(9):
+		print str(i)+")",pythmon[i][0],"	",
+		print str(9 + i) + ")",pythmon[9 + i][0]
 
-# Llista els pythmon en dos columnes per a que el jugador els pugui veure
-for i in range(9):
-	print str(i)+")",pythmon[i][0],"	",
-	print str(9 + i) + ")",pythmon[9 + i][0]
+	# Demana al jugador que esculli 3 pythmon i els guarda
+	for i in range(3):
+		escull = int(input("Escull el teu " + str(i + 1) + " pythmon: "))
+		mipythmon[i] = pythmon[escull][:]
 
-# Demana al jugador que esculli 3 pythmon i els guarda
-for i in range(3):
-	escull = int(input("Escull el teu " + str(i + 1) + " pythmon: "))
-	mipythmon[i] = pythmon[escull][:]
-
-# Mostra els pythmon escollits pel jugador
-for i in range(3):
-		print mipythmon[i][0]
+	# Mostra els pythmon escollits pel jugador
+	for i in range(3):
+			print mipythmon[i][0]
 
 
 time.sleep(2.5)	
-combat = 1
 
-
+print combat
 encombat=[[""],[""]]
 contrari = [[""],[""],[""]] # Espai per als pythmon del oponent
 
+# Aqui es desenvolupa la major part del joc.
 while combat <= 10:
 	clear()
+	
+	# S'escullen els Pythmon del contrari
 	for i in range(3):
 		rnd = random.randint(0,17)
 		contrari[i] = pythmon[rnd][:]
 
 	rotacio = 0
 	encombat[0] = mipythmon[0]
-	encombat[1] = contrari[rotacio]
+	encombat[1] = contrari[rotacio] # Cada cop que es mori un pythmon el contrari canviara al següent
 	
 	# Comença la animació del principi del combat
 	pantalla(Nom + " el teu oponent sera en Marcos", 0)
@@ -218,10 +241,19 @@ while combat <= 10:
 		print "1)",encombat[0][6],"5) Pythmon"
 		torn = int(input())
 		if torn == 5:
+			pantalla("Escull pythmon: ", 3)
 			for i in range(3):
-				print mipythmon[i][0]
-			escull = int(input("Escull pythmon: "))
-			encombat[0] = mipythmon[escull]
+				print str(i+1) + ") " + mipythmon[i][0]
+			escull = int(input())
+			
+			# Controla que la informació introduida sigui correcte i no pugui canviar a un pythmon que esta devilitat
+			while escull < 1 or escull > 3 or mipythmon[escull - 1][2] < 1:
+				pantalla("Siusplau, introdueix un numero correcte: ", 3)
+				for i in range(3):
+					print str(i+1) + ") " + mipythmon[i][0]
+				escull = int(input())
+			encombat[0] = mipythmon[escull - 1]
+			
 		elif torn > 0 and torn < 5:
 			torn+= 5
 			if encombat[0][torn] == "Llançaflames":
@@ -231,16 +263,43 @@ while combat <= 10:
 			elif encombat[0][torn] == "Gigadrenatge":
 				Atac("Planta", 80,encombat,torn)
 		time.sleep(1)
-		if contrari[2][2] < 0:
-			clear()
-			print "\n\n\n\n\n\n\n"
-			print "+-----------------------------------------------------------+"
-			print " Has guanyat"
-			print "+-----------------------------------------------------------+"
+		
+		# Si es devilita tot l'equip del contrari
+		if contrari[2][2] < 1:
+			encombat[1][2] = 0
+			pantalla(encombat[1][0] + " s'ha devilitat",3)
+			time.sleep(1)
+			pantalla("Has guanyat el combat",0)
 			time.sleep(2)
 			break
-		if encombat[1][2] < 1:
+		# Si es devilita un pythmon del contrari
+		elif encombat[1][2] < 1:
+			encombat[1][2] = 0
+			pantalla(encombat[1][0] + " s'ha devilitat",3)
 			rotacio += 1
-			encombat[1] = contrari[rotacio]	
+			encombat[1] = contrari[rotacio]
+			time.sleep(0.5)
+			pantalla("Marcos a tret a " + encombat[1][0],1)
+		encombat[0][2] = 0
+		
+		# Si es devilita tot l'equip del jugador
+		if mipythmon[0][2] < 1 and mipythmon[1][2] < 1 and mipythmon[2][2] < 1:
+			pantalla("Has perdut",0)
+			combat = 10
+			break
+		# Si es devilita un pythmon del jugador
+		elif encombat[0][2] < 1:
+			pantalla("Escull pythmon: ", 3)
+			for i in range(3):
+				print str(i+1) + ") " + mipythmon[i][0]
+			escull = int(input())
+			
+			# Controla que la informació introduida sigui correcte i no pugui canviar a un pythmon que esta devilitat
+			while escull < 1 or escull > 3 or mipythmon[escull - 1][2] < 1:
+				pantalla("Siusplau, introdueix un numero correcte: ", 3)
+				for i in range(3):
+					print str(i+1) + ") " + mipythmon[i][0]
+				escull = int(input())
+			encombat[0] = mipythmon[escull - 1]
 		clear()
 	combat += 1
